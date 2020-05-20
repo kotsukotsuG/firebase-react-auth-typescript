24e3225730307c63a257a98983fae3e8983b341b
kotsukotsuG <hiyorazu+kotsukotsuG@@nts.rioch.co.jp>
2020-05-20 21:31:07 +0900

    Auth.tsxにinterfaceを追加

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -1,10 +1,13 @@
 import React, { FC, useEffect, useState, createContext } from 'react';
 import app from './base';
-import { ContextProps } from './types';
-// interface ContextProps {
-//   currentUser: firebase.User | null;
-// }
-export const AuthContext = createContext<ContextProps>({ currentUser: null });
+
+export interface AuthContextProps {
+  currentUser: firebase.User | null;
+}
+
+export const AuthContext = createContext<AuthContextProps>({
+  currentUser: null,
+});
 
 export const AuthProvider: FC = ({ children }) => {
   const [currentUser, setCurrentUser] = useState<firebase.User | null>(null);
